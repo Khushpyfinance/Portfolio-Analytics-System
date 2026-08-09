9e3f920a79e6a2be899dcb6a1dca17d897b9cc1d
Khush <brahmbhattkhush5@gmail.com>
2026-08-09 14:30:33 -0400

    Remove unrelated practice files

--- Portfolio_Calculator_v1/Solutions.py/Search_missing_element.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def missing_number(self, nums):
-        for i in range(len(nums)+ 1):
-            if i not in nums:
-                return i
-            
-solution = Solution() 
-result = solution.missing_number([1, 0, 3]) 
-print(result)
